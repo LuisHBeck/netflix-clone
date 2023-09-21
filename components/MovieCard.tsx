888a3ca80a69ee6017d91c86af5a73116029ca76
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-21 20:40:22 -0300

    feat: video player

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,17 +2,20 @@
 
 import { BsFillPlayFill } from "react-icons/bs";
 
+import { useRouter } from "next/dist/client/router";
+
 import FavoriteButton from "./FavoriteButton";
 
 interface MovieCardProps {
-  data: Record<string, any>;
+	data: Record<string, any>;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
-  return (
-    <div className="group bg-zinc-900 col-span relative h-[12vw]">
-      <img 
-      className="
+	const router = useRouter();
+	return (
+		<div className="group bg-zinc-900 col-span relative h-[12vw]">
+			<img
+				className="
         cursor-pointer
         object-cover
         transition
@@ -24,10 +27,12 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         delay-300
         w-full
         h-[12vw]
-      " 
-      src={data.thumbnailUrl} alt="Thumnail" />
-      <div
-        className="
+      "
+				src={data.thumbnailUrl}
+				alt="Thumnail"
+			/>
+			<div
+				className="
           opacity-0
           absolute
           top-0
@@ -44,9 +49,9 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
           group-hover:translate-x-[2vw]
           group-hover:opacity-100
         "
-      >
-        <img 
-          className="
+			>
+				<img
+					className="
             cursor-pointer
             object-cover
             transition
@@ -55,11 +60,13 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             rounded-t-md
             w-full
             h-[12vw]
-          " 
-          src={data.thumbnailUrl} alt="Thumnail" />
+          "
+					src={data.thumbnailUrl}
+					alt="Thumnail"
+				/>
 
-          <div 
-          className="
+				<div
+					className="
             z-10
             bg-zinc-800
             p-2
@@ -70,9 +77,11 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             shadow-md
             rounded-b-md
           "
-          >
-            <div className="flex flex-row items-center gap-3">
-              <div onClick={() => {}} className="
+				>
+					<div className="flex flex-row items-center gap-3">
+						<div
+							onClick={() => router.push(`/watch/${data?.id}`)}
+							className="
                 cursor-pointer 
                 w-6 
                 h-6 
@@ -85,24 +94,24 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                 items-center 
                 transition 
                 hover:bg-neutral-300"
-              >
-                <BsFillPlayFill size={30}/>
-              </div>
-              <FavoriteButton movieId={data?.id} />
-            </div>
-            <p className="text-green-400 font-semibold mt-4">
-              New <span className="text-white">New 2023</span>
-            </p>
-            <div className="flex flex-row mt-4 gap-2 items-center">
-              <p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
-            </div>
-            <div className="flex flex-row items-center gap-2 mt-4 text-[8px] text-white lg:text-sm">
-              <p>{data.genre}</p>
-            </div>
-          </div>
-      </div>
-    </div>
-  )
-}
+						>
+							<BsFillPlayFill size={30} />
+						</div>
+						<FavoriteButton movieId={data?.id} />
+					</div>
+					<p className="text-green-400 font-semibold mt-4">
+						New <span className="text-white">New 2023</span>
+					</p>
+					<div className="flex flex-row mt-4 gap-2 items-center">
+						<p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
+					</div>
+					<div className="flex flex-row items-center gap-2 mt-4 text-[8px] text-white lg:text-sm">
+						<p>{data.genre}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default MovieCard
+export default MovieCard;
